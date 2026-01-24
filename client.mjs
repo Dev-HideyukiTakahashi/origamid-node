5d73f78d9ce16423f0c570a6ffa4a4feccf17ec2
Hideyuki <dev.hideyukitakahashi@gmail.com>
2026-01-24 04:56:20 -0300

    aula de node:fs/promises

--- client.mjs
+++ client.mjs
@@ -1,14 +1,53 @@
 // CLIENT
 
-const response = await fetch('http://localhost:3000/produtos?cor=azul', {
+// GET - produto com query
+const notebookResp = await fetch(
+  'http://localhost:3000/produto?categoria=eletronicos&slug=notebook',
+);
+const notebook = await notebookResp.json();
+console.log(notebook);
+
+// GET - lista produtos
+const produtosResp = await fetch('http://localhost:3000/produtos');
+const produtos = await produtosResp.json();
+console.log(produtos);
+
+// POST - novo produto
+const response = await fetch('http://localhost:3000/produtos', {
   method: 'POST',
-  // headers: {
-  //   'Content-Type': 'application/json',
-  // },
-  // body: JSON.stringify({ username: 'John', password: '123456' }),
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    nome: 'notebook',
+    slug: 'notebook',
+    categoria: 'eletronicos',
+    preco: 4000,
+  }),
 });
 
-const body = await response.text();
+await fetch('http://localhost:3000/produtos', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    nome: 'Mesa',
+    slug: 'mesa',
+    categoria: 'moveis',
+    preco: 2000,
+  }),
+});
 
-console.log(body);
-console.log(response);
+await fetch('http://localhost:3000/produtos', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    nome: 'Mouse',
+    slug: 'mouse',
+    categoria: 'eletronicos',
+    preco: 200,
+  }),
+});
